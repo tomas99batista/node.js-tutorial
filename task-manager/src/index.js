bad1d478eee05ab4f6d61d4ca1974508b1530370
tsbatista <tsbatista@ubiwhere.com>
2021-05-04 17:06:23 +0100

    files upload -> ep 123 done

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 require("./db/mongoose");
-const auth = require("./middleware/auth");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
@@ -14,18 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
-
-// const Task = require("./models/task");
-// const User = require("./models/user");
-
-// const main = async () => {
-//   // const task = await Task.findById("60912cfc8aebc2eb881a597a");
-//   // await task.populate("owner").execPopulate();
-//   // console.log(task.owner);
-
-//   const user = await User.findById("60912c56db4290d72e9c5cda");
-//   await user.populate("tasks").execPopulate();
-//   console.log(user.tasks);
-// };
-
-// main();
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const multer = require("multer");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
+
+const upload = multer({
+  dest: "avatars",
+});
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -87,4 +92,10 @@ router.delete("/users/me", auth, async (req, res) => {
     res.status(500).send(e);
   }
 });
+
+router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
+  console.log("here");
+  res.send();
+});
+
 module.exports = router;
