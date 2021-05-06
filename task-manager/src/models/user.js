81fa2956bb3a11616c5c6862c9c21573618a5b79
tsbatista <tsbatista@ubiwhere.com>
2021-05-06 16:30:47 +0100

    finish section 14

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -46,6 +46,9 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
+    avatar: {
+      type: Buffer,
+    },
   },
   {
     timestamps: true,
@@ -64,6 +67,7 @@ userSchema.methods.toJSON = function () {
 
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
 
   return userObject;
 };
